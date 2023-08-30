47357f415facad419f4ee3f0f2ba8fe9bbd8bc01
llevasse <leonardlevasseur@yahoo.com>
2023-08-30 10:20:21 +0200

    init Makefile

---
+++ philosophers/headers/philosophers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/30 09:54:38 by llevasse          #+#    #+#             */
+/*   Updated: 2023/08/30 09:57:51 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+
+#endif
