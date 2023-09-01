4bd2dc6353e01319dae222898d1660b5b45d28ab
Leo <leonardlevasseur@yahoo.com>
2023-09-01 12:44:30 +0200

    print right time

--- philosophers/srcs/routine.c
+++ philosophers/srcs/routine.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 00:25:40 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/01 12:36:32 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/01 12:43:47 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	can_eat(t_philo *buddy)
 {
 	pthread_mutex_lock(&buddy->left_buddy->fork);
 	gettimeofday(&buddy->current_time, NULL);
-	ft_putnbr_fd(buddy->table->init_time.tv_usec - buddy->current_time.tv_usec, 1);
+	ft_putnbr_fd(buddy->current_time.tv_usec - buddy->table->init_time.tv_usec, 1);
 	ft_putstr_fd(" ", 1);
 	ft_putnbr_fd(buddy->id, 1);
 	ft_putstr_fd(" has taken a fork\n", 1);
 	pthread_mutex_lock(&buddy->right_buddy->fork);
-	ft_putnbr_fd(buddy->table->init_time.tv_usec - buddy->current_time.tv_usec, 1);
+	ft_putnbr_fd(buddy->current_time.tv_usec - buddy->table->init_time.tv_usec, 1);
 	ft_putstr_fd(" ", 1);
 	ft_putnbr_fd(buddy->id, 1);
 	ft_putstr_fd(" has taken a fork\n", 1);
@@ -33,11 +33,12 @@ void	*alive_routine(void	*args)
 	t_philo	*buddy;
 
 	buddy = (t_philo *)args;
-	print_philo(buddy);
 	if (can_eat(buddy))
 	{
 		usleep(buddy->time_to_eat);
 		gettimeofday(&buddy->time_since_eating, NULL);
+		pthread_mutex_unlock(&buddy->right_buddy->fork);
+		pthread_mutex_unlock(&buddy->right_buddy->fork);
 	}
 	pthread_exit(NULL);
 	return (NULL);
