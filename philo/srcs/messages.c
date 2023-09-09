64d4ee933cccc6975c619be88f63ffc101a0e067
llevasse <leonardlevasseur@yahoo.com>
2023-09-09 17:10:22 +0200

    try to optimize run time by removing useless expression

--- philo/srcs/messages.c
+++ philo/srcs/messages.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 22:35:37 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/09 16:05:28 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/09 17:09:07 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,48 @@
 
 void	print_take_fork(t_philo *buddy)
 {
-	long long	time;
-
 	if (!check_death(buddy))
 		return ;
 	pthread_mutex_lock(&buddy->table->write);
-	time = timestamp() - buddy->table->init_time;
-	printf("%lld %d has taken a fork\n", time, buddy->id + 1);
+	printf("%lld %d has taken a fork\n", timestamp() - buddy->table->init_time, buddy->id + 1);
 	pthread_mutex_unlock(&buddy->table->write);
 }
 
 void	print_eat(t_philo *buddy)
 {
-	long long	time;
-
 	if (!check_death(buddy))
 		return ;
 	pthread_mutex_lock(&buddy->table->write);
-	time = buddy->time_since_eat - buddy->table->init_time;
-	printf("%lld %d\033[0;32m is eating\033[0m\n", time, buddy->id + 1);
+	printf("%lld %d\033[0;32m is eating\033[0m\n", timestamp() - buddy->table->init_time, buddy->id + 1);
 	buddy->eaten_times++;
 	pthread_mutex_unlock(&buddy->table->write);
 }
 
 void	print_sleep(t_philo *buddy)
 {
-	long long	time;
-
 	if (!check_death(buddy))
 		return ;
 	pthread_mutex_lock(&buddy->table->write);
-	time = timestamp() - buddy->table->init_time;
-	printf("%lld %d\033[0;33m is sleeping\033[0m\n", time, buddy->id + 1);
+	printf("%lld %d\033[0;33m is sleeping\033[0m\n", timestamp() - buddy->table->init_time, buddy->id + 1);
 	pthread_mutex_unlock(&buddy->table->write);
 }
 
 void	print_think(t_philo *buddy)
 {
-	long long	time;
-
 	if (!check_death(buddy))
 		return ;
 	pthread_mutex_lock(&buddy->table->write);
-	time = timestamp() - buddy->table->init_time;
-	printf("%lld %d is thinking\n", time, buddy->id + 1);
+	printf("%lld %d is thinking\n", timestamp() - buddy->table->init_time, buddy->id + 1);
 	pthread_mutex_unlock(&buddy->table->write);
 }
 
 void	print_died(t_philo *buddy)
 {
-	long long	time;
-
 	pthread_mutex_lock(&buddy->table->read);
 	buddy->is_alive = 0;
 	buddy->table->alive = 0;
 	pthread_mutex_unlock(&buddy->table->read);
 	pthread_mutex_lock(&buddy->table->write);
-	time = timestamp() - buddy->table->init_time;
-	printf("%lld %d\033[0;31m died\033[0m\n", time, buddy->id + 1);
+	printf("%lld %d\033[0;31m died\033[0m\n", timestamp() - buddy->table->init_time, buddy->id + 1);
 	pthread_mutex_unlock(&buddy->table->write);
 }
