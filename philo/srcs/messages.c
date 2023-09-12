da13e47b6f003ca6205aef7a5e05d95df27a3ef1
Leo <leonardlevasseur@yahoo.com>
2023-09-12 11:17:50 +0200

    rm possible lock order inversion

--- philo/srcs/messages.c
+++ philo/srcs/messages.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 22:35:37 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/12 11:13:10 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/12 11:17:44 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ void	print_take_fork(t_philo *buddy, long long time)
 
 void	print_eat(t_philo *buddy, long long time)
 {
-	pthread_mutex_lock(&buddy->table->write);
 	time = timestamp(buddy->curr_time) - buddy->table->init_time;
 	if (time >= 999999)
 		time = 999999;
+	pthread_mutex_lock(&buddy->table->write);
 	if (!check_death(buddy, time))
 		return ;
 	printf("%lld %d\033[0;32m is eating\033[0m\n", time, buddy->id + 1);
@@ -41,10 +41,10 @@ void	print_eat(t_philo *buddy, long long time)
 
 void	print_sleep(t_philo *buddy, long long time)
 {
-	pthread_mutex_lock(&buddy->table->write);
 	time = timestamp(buddy->curr_time) - buddy->table->init_time;
 	if (time >= 999999)
 		time = 999999;
+	pthread_mutex_lock(&buddy->table->write);
 	if (!check_death(buddy, time))
 		return ;
 	printf("%lld %d\033[0;33m is sleeping\033[0m\n", time, buddy->id + 1);
@@ -53,10 +53,10 @@ void	print_sleep(t_philo *buddy, long long time)
 
 void	print_think(t_philo *buddy, long long time)
 {
-	pthread_mutex_lock(&buddy->table->write);
 	time = timestamp(buddy->curr_time) - buddy->table->init_time;
 	if (time >= 999999)
 		time = 999999;
+	pthread_mutex_lock(&buddy->table->write);
 	if (!check_death(buddy, time))
 		return ;
 	printf("%lld %d is thinking\n", time, buddy->id + 1);
