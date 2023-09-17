1c56eeeab45091563bd479b1447e85faca42691d
Leo <leonardlevasseur@yahoo.com>
2023-09-17 19:29:22 +0200

    fix check_died still looping if eaten->times is specified

--- philo/srcs/death_routine.c
+++ philo/srcs/death_routine.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 00:25:40 by llevasse          #+#    #+#             */
-/*   Updated: 2023/09/17 19:16:45 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/09/17 19:28:44 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,25 @@ void	detach_threads(t_table *table)
 	i = 0;
 	while (i < table->nb_philo)
 		pthread_detach(table->threads[i++]);
+}
+
+static int	check_need_eat(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	while (i < table->nb_philo)
+	{
+		pthread_mutex_lock(&table->philo[i]->eat);
+		if (table->philo[i]->eaten_times > 0 || table->philo[i]->eaten_times == -1)
+		{
+			pthread_mutex_unlock(&table->philo[i]->eat);
+			return (1);
+		}
+		pthread_mutex_unlock(&table->philo[i]->eat);
+		i++;
+	}
+	return (0);
 }
 
 static int	check_died(t_table *table)
@@ -55,9 +74,11 @@ void	*death_routine(void	*args)
 	while (1)
 	{
 		pthread_mutex_lock(&table->read);
-		if (check_died(table) == 0)
+		if (!check_died(table))
 			break ;
 		pthread_mutex_unlock(&table->read);
+		if (!check_need_eat(table))
+			break ;
 		usleep(100);
 	}
 	pthread_exit(NULL);
